27ed09bba7d79cbfc1c3adb59c2a8b49121a57fb
Tirtadwipa Manunggal <tirtadwipa.manunggal@gmail.com>
2020-03-16 10:06:03 +0700

    Updates

---
+++ precache-manifest.a83f3710bdc38087a1b51dbd3c97f38a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8385436348c320ba467f3bcdbf95670b",
+    "url": "/outbreak-simulator/index.html"
+  },
+  {
+    "revision": "dd008d2ffd01c212ccb8",
+    "url": "/outbreak-simulator/static/css/main.b7677955.chunk.css"
+  },
+  {
+    "revision": "47da1d2dddbe75f10259",
+    "url": "/outbreak-simulator/static/js/2.756ee00c.chunk.js"
+  },
+  {
+    "revision": "1e8a389978a91a156d8e2868c1e7255c",
+    "url": "/outbreak-simulator/static/js/2.756ee00c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd008d2ffd01c212ccb8",
+    "url": "/outbreak-simulator/static/js/main.d3a3b0e8.chunk.js"
+  },
+  {
+    "revision": "f808223dcc75c4e08975",
+    "url": "/outbreak-simulator/static/js/runtime-main.0c433f6c.js"
+  }
+]);
